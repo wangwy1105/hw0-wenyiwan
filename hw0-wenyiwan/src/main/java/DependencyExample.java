8b67cb626a1b2dd56daeac4cdee67598edf611ff
wangwy1105 <wangwy1105@gmail.com>
2013-08-29 16:49:59 -0400

    3rd commit

--- hw0-wenyiwan/src/main/java/DependencyExample.java
+++ hw0-wenyiwan/src/main/java/DependencyExample.java
@@ -19,9 +19,7 @@ public class DependencyExample {
 */
 
  public static void main(String[] args) {
-    TokenizerFactory<Word> factory =
-        PTBTokenizerFactory.newTokenizerFactory();
-    Tokenizer<Word> tokenizer =
-        factory.getTokenizer(new StringReader(args[0]));
+    TokenizerFactory<Word> factory = PTBTokenizerFactory.newTokenizerFactory();
+    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(args[0]));
     System.out.println(tokenizer.tokenize());
 } }
